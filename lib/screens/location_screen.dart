b07889a76fe9793813779604bb7c8a353d8d8761
JUSTICE <justicenwogu8@gmail.com>
2022-07-23 23:54:21 +0100

    first commit

---
+++ lib/screens/location_screen.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:clima/utilities/constants.dart';
+import 'package:clima/services/weather.dart';
+
+
+class LocationScreen extends StatefulWidget {
+  LocationScreen({this.locationWeather});
+
+  final locationWeather;
+
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+   WeatherModel weather = WeatherModel();
+
+  var temperature = 0;
+  String weatherIcon = '' ;
+  String cityName = '';
+  String weatherMessage = '';
+
+  @override
+  void initState() {
+    super.initState();
+
+    updateUI(widget.locationWeather);
+  }
+
+
+  void updateUI(dynamic weatherData){
+    setState(() {
+      if (weatherData == null){
+        temperature = 0;
+        weatherIcon = 'Error';
+        weatherMessage = 'unable to get weather data';
+        cityName = '';
+        return;
+      }
+  var temp = weatherData['main']['temp'];
+  temperature = temp.toInt();
+  var conditionNumber = weatherData['weather'][0]['id'];
+  weatherIcon = weather.getWeatherIcon(conditionNumber);
+  weatherMessage = weather.getMessage(temperature);
+  cityName = weatherData['name'];
+  });
+    
+  
+  
+
+  print(temperature);
+
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: const AssetImage('images/location_background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.dstATop),
+          ),
+        ),
+        constraints: const BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () async{
+                      var weatherData = await weather.getLocationWeather();
+                      updateUI(weatherData);
+                    },
+                    child: const Icon(
+                      Icons.near_me,
+                      size: 50.0,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Icon(
+                      Icons.location_city,
+                      size: 50.0,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '$temperature°',
+                      style: kTempTextStyle,
+                    ),
+                    Text(
+                      weatherIcon,
+                      style: kConditionTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+               Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: Text(
+                  '$weatherMessage in $cityName!',
+                  textAlign: TextAlign.right,
+                  style: kMessageTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
---
+++ lib/services/location.dart
@@ -0,0 +1,24 @@
+import 'package:geolocator/geolocator.dart';
+
+class Location {
+  Location(this.latitude, this.longitude);
+ double latitude;
+ double longitude;
+
+Future<void> getCurrentLocation()async{
+  try{
+    Position position = await  Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+
+latitude = position.latitude;
+longitude = position.longitude;
+print(latitude);
+}
+
+catch(e){
+  print(e);
+}
+
+}
+}
+
+  
